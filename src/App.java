fa7112afbbda85c1b87b8a5b87b08ccf71d0afeb
Roy Montemayor <rfm5@rice.edu>
2023-01-22 22:20:02 -0600

    Styling and Exceptions

--- src/App.java
+++ src/App.java
@@ -1,107 +1,115 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.File;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-
-
+import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        File f = new File(
-            "/Users/roymontemayor/Desktop/Rice/Spring 2023/COMP 380/Homework 1/BlackJack/src/COMP_380_HW_1_Example_CSV.csv");
-        File newFile = new File("/Users/roymontemayor/Desktop/Rice/Spring 2023/COMP 380/Homework 1/COMP_380_HW_1_Solutions.csv");
 
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(newFile));
+    public static void bjCalc(File inFile, File outFile) throws IOException, NullPointerException{
+        BufferedReader br = null;
+        BufferedWriter bw = null;
 
-        String line;
+        try {
+            br = new BufferedReader(new FileReader(inFile));
+            bw = new BufferedWriter(new FileWriter(outFile));
 
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-            String[] columns = line.split(",");
-            String dealerCard = line.substring(1, 6);
-            int ourStart = 0;
-            int finishSearch = 0;
-            while (ourStart < line.length() && finishSearch < 8) {
-                if (line.charAt(ourStart) == ',') {
-                    ourStart++;
-                    finishSearch++;
-                } else if (line.charAt(ourStart) == '1') {
-                    ourStart += 6;
-                    finishSearch++;
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                //            System.out.println(line);
+                String[] columns = line.split(",");
+//                String dealerCard = line.substring(1, 6);
+                int ourStart = 0;
+                int finishSearch = 0;
+                while (ourStart < line.length() && finishSearch < 8) {
+                    if (line.charAt(ourStart) == ',') {
+                        ourStart++;
+                        finishSearch++;
+                    } else if (line.charAt(ourStart) == '1') {
+                        ourStart += 6;
+                        finishSearch++;
+                    }
+                    // System.out.println(line.charAt(ourStart));
+                    // System.out.println(ourStart + " " + finishSearch);
                 }
-                // System.out.println(line.charAt(ourStart));
-                // System.out.println(ourStart + " " + finishSearch);
-            }
-            String ourCard = line.substring(ourStart);
-            while (ourCard.charAt(ourCard.length() - 1) == ',') {
-                if (ourCard.charAt(ourCard.length() - 1) == ',') {
-                    ourCard = ourCard.substring(0, ourCard.length() - 1);
+                String ourCard = line.substring(ourStart);
+                while (ourCard.charAt(ourCard.length() - 1) == ',') {
+                    if (ourCard.charAt(ourCard.length() - 1) == ',') {
+                        ourCard = ourCard.substring(0, ourCard.length() - 1);
+                    }
                 }
-            }
 
-            //    ourCard = "," + ourCard;
-            //           System.out.println(ourCard);
-            String dealerSuit = String.valueOf(dealerCard.charAt(3));
-            String ourSuit = "";
 
-            String dealerVal = String.valueOf(dealerCard.charAt(4));
-            String ourVal = "";
+//                String dealerSuit = String.valueOf(dealerCard.charAt(3));
+//                String ourSuit = "";
 
-            int adjust = 0;
-            boolean canAdd = true;
-            for (int chr = 0; chr < ourCard.length(); chr++) {
-                if (ourCard.charAt(chr) == ',') {
-                    adjust += 1;
-                    canAdd = true;
+//                String dealerVal = String.valueOf(dealerCard.charAt(4));
+                String ourVal = "";
+
+                int adjust = 0;
+                boolean canAdd = true;
+                for (int chr = 0; chr < ourCard.length(); chr++) {
+                    if (ourCard.charAt(chr) == ',') {
+                        adjust += 1;
+                        canAdd = true;
+                    }
+                    if (canAdd) {
+//                        ourSuit += ourCard.charAt(adjust * 6 + 3);
+                        ourVal += ourCard.charAt(adjust * 6 + 4);
+                        canAdd = false;
+                    }
                 }
-                if (canAdd == true) {
-                    ourSuit += ourCard.charAt(adjust * 6 + 3);
-                    ourVal += ourCard.charAt(adjust * 6 + 4);
-                    canAdd = false;
-                }
-            }
 
-            int softHandTotalVal = 0;
-            int hardHandTotalVal = 0;
-            for (int j = 0; j < ourVal.length(); j++) {
-                if (ourVal.charAt(j) == '1') {
-                    hardHandTotalVal += 1;
-                    softHandTotalVal += 11;
-                } else if (ourVal.charAt(j) == 'A' || ourVal.charAt(j) == 'J' || ourVal.charAt(j) == 'D'
-                    || ourVal.charAt(j) == 'E' || ourVal.charAt(j) == 'K') {
-                    hardHandTotalVal += 10;
-                    softHandTotalVal += 10;
-                } else {
-                    hardHandTotalVal +=
-                        Integer.parseInt(String.valueOf(ourVal.charAt(j)));
-                    softHandTotalVal +=
-                        Integer.parseInt(String.valueOf(ourVal.charAt(j)));
+                int softHandTotalVal = 0;
+                int hardHandTotalVal = 0;
+                for (int j = 0; j < ourVal.length(); j++) {
+                    if (ourVal.charAt(j) == '1') {
+                        hardHandTotalVal += 1;
+                        softHandTotalVal += 11;
+                    } else if (ourVal.charAt(j) == 'A' || ourVal.charAt(j) == 'J'
+                        || ourVal.charAt(j) == 'D'
+                        || ourVal.charAt(j) == 'E' || ourVal.charAt(j) == 'K') {
+                        hardHandTotalVal += 10;
+                        softHandTotalVal += 10;
+                    } else {
+                        hardHandTotalVal +=
+                            Integer.parseInt(String.valueOf(ourVal.charAt(j)));
+                        softHandTotalVal +=
+                            Integer.parseInt(String.valueOf(ourVal.charAt(j)));
+                    }
                 }
-            }
-            System.out.println("hard:" + hardHandTotalVal + " soft: " + softHandTotalVal);
+                //            System.out.println("hard:" + hardHandTotalVal + " soft: " + softHandTotalVal);
 
-            if (hardHandTotalVal > 11) {
-                System.out.println("STAY");
-                columns[0] = "STAY";
-            } else {
-                if (softHandTotalVal > 17) {
-                    System.out.println("STAY");
+                if (hardHandTotalVal > 11) {
                     columns[0] = "STAY";
                 } else {
-                    System.out.println("HIT");
-                    columns[0] = "HIT";
+                    if (softHandTotalVal > 17) {
+                        columns[0] = "STAY";
+                    } else {
+                        columns[0] = "HIT";
+                    }
                 }
+
+                bw.write(String.join(",", columns));
+                bw.newLine();
+
             }
-
-            bw.write(String.join(",", columns));
-            bw.newLine();
-
+        } catch(IOException e){
+            System.out.println("Error opening or writing to file: " + e);
+        } catch(NullPointerException n){
+            System.out.println("Null Pointer Exception: " + n);
         }
 
         br.close();
         bw.close();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Define in and out file absolute paths
+        File inFile = new File(
+            "/Users/roymontemayor/Desktop/Rice/Spring 2023/COMP 380/Homework 1/BlackJack/src/COMP_380_HW_1_Example_CSV.csv");
+        File outFile = new File(
+            "/Users/roymontemayor/Desktop/Rice/Spring 2023/COMP 380/Homework 1/COMP_380_HW_1_Solutions.csv");
+
+        bjCalc(inFile, outFile);
 
     }
 }
